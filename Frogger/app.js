a35eb62d18bf0bdd603dc874a6373c5659dfb99c
Jenniline <jennilineebai@gmail.com>
2022-04-07 15:35:26 +0100

    WIP:New Game Frogger

---
+++ Frogger/app.js
@@ -0,0 +1,27 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const result = document.querySelector('#result')
+const startPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+let currentIndex = 76;
+console.log('me')
+
+function moveFrog(e) {
+
+    switch(e.key) {
+        case 'ArrowLeft' :
+            console.log('move left')
+            break
+        case 'ArrowRight' :
+            console.log('move right')
+            break
+        case 'ArrowUp' :
+            console.log('move up')
+            break
+        case 'ArrowDown' :
+            console.log('move down')
+            break
+    }
+    squares[currentIndex].classList.add('frog')
+}
+
+document.addEventListener('keyup', moveFrog)
